8b5f633a9cf44ad0de8e70fc3e17c319e7b93a65
evgshat <eoshatalyuk@sberbank.ru>
2022-07-02 15:02:48 +0300

    stack need check

--- ft_stack.hpp
+++ ft_stack.hpp
@@ -12,24 +12,92 @@
 #include <stack>
 #include <deque>
 
+// надо ли везде добавлять ft?
 template <class _T, class _Container = std::deque<_T> >
 class ft_stack
 {
 	public:
 		typedef _T			value_type;
 		typedef _Container	container_type;
+		typedef size		size_type;
 	protected:
 		container_type c;
 	public:
-		ft_stack() {}
-		void push(const value_type& other) { c.push_back(other); }
-		bool empty() const { c.empty(); }
-		value_type top() {return c.back();} // обязательна ссылка в возвращаемом значении?
-		// ft_stack(const ft_stack &other) : c(other.c) {}
-		size
+		// что такое explicit?
+		explicit ft_stack(const container_type& ct = container_type()): c(ct) {}
+		bool ft_empty() const
+		{
+			return c.empty();
+		}
+		size_type ft_size() const
+		{
+			return c.size();
+		}
+		value_type& ft_top() const
+		{
+			return c.back();
+		}
+		// почему два метода?
+		const value_type& ft_top() const
+		{
+			return c.back();
+		}
+		void ft_push(const value_type& el)
+		{
+			return c.push_back(el);
+		}
+		void ft_pop ()
+		{
+			return c.pop_back();
+		}
 
+		template <class _T1, class _Container1>
+		// что такое friend?
+		friend bool operator== (const stack<_T1, _Container1>& st1, const stack<_T1, _Container1>& st2);
 
-
+		template <class _T1, class _Container1>
+		friend bool operator< (const stack<_T1, _Container1>& st1, const stack<_T1, _Container1>& st2);
 };
+
+// почему внутри класса писали те же шаблонные ф-ии? потому что там friend - зачем он?
+// точно ли надо ставить "_" перед T1 и Container? Для чего это вообще?
+// что значат шаблонные ф-ии внутри класса и вне? - вне - чтобы вне класса использовать...
+// разве ф-ии не должны быть константными?
+
+template <class _T1, class _Container1>
+bool operator== (const ft_stack<_T1, _Container1>& st1, const ft_stack<_T1, _Container1>& st2)
+{
+	return st1 == st2; // (.c) -- ? у меня неправильный вариант
+}
+
+template <class _T1, class _Container1>
+bool operator!= (const ft_stack<_T1, _Container1>& st1, const ft_stack<_T1, _Container1>& st2)
+{
+	return !(st1 == st2);
+}
+
+template <class _T1, class _Container1>
+bool operator< (const ft_stack<_T1, _Container1>& st1, const ft_stack<_T1, _Container1>& st2)
+{
+	return st1 < st2; // (.c) -- ? у меня неправильный вариант
+}
+
+template <class _T1, class _Container1>
+bool operator<= (const ft_stack<_T1, _Container1>& st1, const ft_stack<_T1, _Container1>& st2)
+{
+	return st1 < st2; // перепроверить
+}
+
+template <class _T1, class _Container1>
+bool operator> (const ft_stack<_T1, _Container1>& st1, const ft_stack<_T1, _Container1>& st2)
+{
+	return st1 > st2; // перепроверить
+}
+
+template <class _T1, class _Container1>
+bool operator>= (const ft_stack<_T1, _Container1>& st1, const ft_stack<_T1, _Container1>& st2)
+{
+	return st1 > st2; // перепроверить
+}
 
 #endif
