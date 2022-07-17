eb48c039d8fcb957b1f38a22a6f198f8609fd4d7
Laucher Charlette <lcharlet@at-d3.msk.21-school.ru>
2022-07-17 18:58:21 +0300

    fix bugs

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include "ft_containers.hpp"
+#include <iostream>
+#include <vector>
+#include <deque>
+
+void test1()
+{
+	// int a;
+	std::deque<int> d(1);
+	d.push_back(44);
+	std::cout<< d[1] <<std::endl;
+
+	ft_stack<int> st;
+	st.ft_push(222);
+	std::cout<< st.ft_top() <<std::endl;
+}
+
+void main()
+{
+	test1();
+	return 0;
+}
